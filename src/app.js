2d05276c149d20c7ed04491c3a57bab30f62d030
anuragmy <anuragmy2729@gmail.com>
2019-03-10 14:13:11 +0530

    production evn added

--- src/app.js
+++ src/app.js
@@ -9,19 +9,21 @@ import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
 const store = configureStore();
-
-store.dispatch(addExpense({ description: "Water bill", amount: 4500 }));
-store.dispatch(addExpense({ description: "Gas bill", amount: 50 }));
-store.dispatch(addExpense({ description: "electricity bill", amount: 100 }));
-store.dispatch(addExpense({ description: "phone bill", amount: 6000 }));
-store.dispatch(addExpense({ description: "mobile bill", amount: 630 }));
-store.dispatch(addExpense({ description: "parking bill", amount: 2000 }));
-store.dispatch(addExpense({ description: "wi-fi bill", amount: 3400 }));
+//dummy data
+// store.dispatch(addExpense({ description: "Water bill", amount: 4500 }));
+// store.dispatch(addExpense({ description: "Gas bill", amount: 50 }));
+// store.dispatch(addExpense({ description: "electricity bill", amount: 100 }));
+// store.dispatch(addExpense({ description: "phone bill", amount: 6000 }));
+// store.dispatch(addExpense({ description: "mobile bill", amount: 630 }));
+// store.dispatch(addExpense({ description: "parking bill", amount: 2000 }));
+// store.dispatch(addExpense({ description: "wi-fi bill", amount: 3400 }));
 //store.dispatch(setTextFilter("water"));
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 console.log(visibleExpenses);
+
+console.log("test");
 
 const jsx = (
   <Provider store={store}>
