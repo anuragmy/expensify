8e97608d174b1f3c2e919040a06a20576f36ca87
anuragmy <anuragmy2729@gmail.com>
2019-03-10 20:04:52 +0530

    time and currency format changed

--- src/components/ExpenseListItems.js
+++ src/components/ExpenseListItems.js
@@ -4,7 +4,8 @@ import { connect } from "react-redux";
 import { removeExpense } from "../actions/expenses";
 import { editExpense } from "../actions/expenses";
 import { Link } from "react-router-dom";
-
+import moment from "moment";
+import numeral from "numeral";
 const ExpenseListItems = ({ dispatch, id, description, amount, createdAt }) => {
   return (
     <div>
@@ -12,7 +13,8 @@ const ExpenseListItems = ({ dispatch, id, description, amount, createdAt }) => {
         <h3>{description}</h3>
       </Link>
       <p>
-        {amount}-{createdAt}
+        {numeral(amount).format("$0,0.00")}-
+        {moment(createdAt).format("MMMM Do, YYYY")}
       </p>
     </div>
   );
